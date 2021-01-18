42629e0532024c532ad1d064fa55845825a06b0c
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-01-18 18:59:16 +0100

    Create users.go

---
+++ pkg/ftapi/users.go
@@ -0,0 +1,13 @@
+package ftapi
+
+// User represents a user entity
+type User struct {
+	ID string `json:"id,omitempty"`
+	Login string `json:"login,omitempty"`
+	Email string `json:"email"`
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	Kind string `json:"kind"`
+	CampusID int `json:"campus_id"`
+	Url string `json:"url,omitempty"`
+}
