734214af16ab50d07fc8a7edbbfe48d5ae6f87d8
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-01-18 19:00:15 +0100

    WIP: create create.go

---
+++ cmd/create.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"goft/pkg/ftapi"
+	"net/http"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// userCreateCmd represents the create command
+var userCreateCmd = &cobra.Command{
+	Use:   "create email first_name last_name kind campus_id",
+	Short: "Create a new user",
+	Long: `Create a new user account.
+This command requires the Advanced tutor role
+No password is set, the user should reset his password using the web interface.
+
+kind must be either admin, student or external.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 5 {
+			return fmt.Errorf("accepts 5 arg(s), received %d", len(args))
+		}
+		if args[3] != "admin" && args[3] != "student" && args[3] != "external" {
+			return errors.New("kind must be admin, student or external")
+		}
+		campusID, err := strconv.Atoi(args[4])
+		if err != nil || campusID <= 0 {
+			return errors.New("invalid campus_id")
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		campusID, _ := strconv.Atoi(args[4])
+		user := ftapi.User{
+			Email:     args[0],
+			FirstName: args[1],
+			LastName:  args[2],
+			Kind:      args[3],
+			CampusID:  campusID,
+		}
+		login, _ := cmd.Flags().GetString("login")
+		if login != "" {
+			user.Login = login
+		}
+		ftAPI := ftapi.New("lo", &http.Client{})
+		_ = ftAPI.CreateUser(&user)
+		return nil
+	},
+}
+
+func init() {
+	usersCmd.AddCommand(userCreateCmd)
+
+	// login - password
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// userCreateCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// userCreateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	userCreateCmd.Flags().String("login", "", "Set a custom login, leave empty to let the intra generate one")
+}
