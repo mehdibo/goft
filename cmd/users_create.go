b8747bf58c0d8c8ec1f3469e20e2b7a56feccab1
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-01-19 17:03:35 +0100

    Use constructor for userCreateCmd

--- cmd/users_create.go
+++ cmd/users_create.go
@@ -11,45 +11,48 @@ import (
 )
 
 // userCreateCmd represents the create command
-var userCreateCmd = &cobra.Command{
-	Use:   "create email first_name last_name kind campus_id",
-	Short: "Create a new user",
-	Long: `Create a new user account.
+func NewUserCreateCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "create email first_name last_name kind campus_id",
+		Short: "Create a new user",
+		Long: `Create a new user account.
 This command requires the Advanced tutor role
 No password is set, the user should reset his password using the web interface.
 
 kind must be either admin, student or external.`,
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 5 {
-			return fmt.Errorf("accepts 5 arg(s), received %d", len(args))
-		}
-		if args[3] != "admin" && args[3] != "student" && args[3] != "external" {
-			return errors.New("kind must be admin, student or external")
-		}
-		campusID, err := strconv.Atoi(args[4])
-		if err != nil || campusID <= 0 {
-			return errors.New("invalid campus_id")
-		}
-		return nil
-	},
-	RunE: func(cmd *cobra.Command, args []string) error {
-		campusID, _ := strconv.Atoi(args[4])
-		user := ftapi.User{
-			Email:     args[0],
-			FirstName: args[1],
-			LastName:  args[2],
-			Kind:      args[3],
-			CampusID:  campusID,
-		}
-		login, _ := cmd.Flags().GetString("login")
-		if login != "" {
-			user.Login = login
-		}
-		ftAPI := ftapi.New("lo", &http.Client{})
-		_ = ftAPI.CreateUser(&user)
-		return nil
-	},
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 5 {
+				return fmt.Errorf("accepts 5 arg(s), received %d", len(args))
+			}
+			if args[3] != "admin" && args[3] != "student" && args[3] != "external" {
+				return errors.New("kind must be admin, student or external")
+			}
+			campusID, err := strconv.Atoi(args[4])
+			if err != nil || campusID <= 0 {
+				return errors.New("invalid campus_id")
+			}
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			campusID, _ := strconv.Atoi(args[4])
+			user := ftapi.User{
+				Email:     args[0],
+				FirstName: args[1],
+				LastName:  args[2],
+				Kind:      args[3],
+				CampusID:  campusID,
+			}
+			login, _ := cmd.Flags().GetString("login")
+			if login != "" {
+				user.Login = login
+			}
+			ftAPI := ftapi.New("lo", &http.Client{})
+			_ = ftAPI.CreateUser(&user)
+			return nil
+		},
+	}
 }
+var userCreateCmd = NewUserCreateCmd()
 
 func init() {
 	usersCmd.AddCommand(userCreateCmd)
