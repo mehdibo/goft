83bfd38beb3e98de52a6ef897ee74b303505bfaf
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-01-22 00:17:33 +0100

    Add Close entity

--- pkg/ftapi/entities.go
+++ pkg/ftapi/entities.go
@@ -1,5 +1,7 @@
 package ftapi
 
+import "time"
+
 // User represents a user entity
 type User struct {
 	ID int `json:"id,omitempty"`
@@ -10,4 +12,27 @@ type User struct {
 	Kind string `json:"kind"`
 	CampusID int `json:"campus_id"`
 	URL string `json:"url,omitempty"`
+}
+
+type communityService struct {
+	ID int `json:"id,omitempty"`
+	Duration int64 `json:"duration,omitempty"`
+	ScheduledAt time.Time `json:"schedule_at,omitempty"`
+	Occupation string `json:"occupation,omitempty"`
+	State string `json:"state,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
+}
+
+// Close represents a close
+type Close struct {
+	ID int `json:"id,omitempty"`
+	Kind string `json:"kind,omitempty"`
+	Reason string `json:"reason,omitempty"`
+	State string `json:"state,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
+	CommunityServices []*communityService `json:"community_services,omitempty"`
+	User *User `json:"user,omitempty"`
+	Closer *User `json:"closer,omitempty"`
 }
