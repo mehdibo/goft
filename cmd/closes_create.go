923076fbb55086b70af47d5b5f31a35e672fd447
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-01-22 14:10:46 +0100

    Implement closes create

--- cmd/closes_create.go
+++ cmd/closes_create.go
@@ -6,6 +6,20 @@ import (
 	"goft/pkg/ftapi"
 )
 
+func isValidKind(kind string) bool {
+	switch kind {
+	case "agu",
+		"black_hole",
+		"deserter",
+		"non_admitted",
+		"serious_misconduct",
+		"social_security",
+		"other":
+		return true
+	}
+	return false
+}
+
 // NewCloseCreateCmd Create the close create cmd
 func NewCloseCreateCmd(api *ftapi.APIInterface) *cobra.Command {
 	// login - kind - reason -
@@ -15,16 +29,25 @@ func NewCloseCreateCmd(api *ftapi.APIInterface) *cobra.Command {
 		Long: `This command requires the Basic staff role
 
 kind must be one of the following options: agu, black_hole, deserter, non_admitted, serious_misconduct, social_security or other`,
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("create called")
-			(*api).CreateClose(&ftapi.Close{
-				Kind:              "other",
-				Reason:            "This is the reason",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 3 {
+				return fmt.Errorf("accepts 3 arg(s), received %d", len(args))
+			}
+			if !isValidKind(args[1]) {
+				return fmt.Errorf("'%s' is not a valid kind", args[1])
+			}
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			err := (*api).CreateClose(&ftapi.Close{
+				Kind: args[1],
+				Reason: args[2],
 				CommunityServices: nil,
-				User:              &ftapi.User{
-					Login:     "spoody",
+				User: &ftapi.User{
+					Login: args[3],
 				},
 			})
+			return err
 		},
 	}
 }
