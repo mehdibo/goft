c24965b0d864679a3fa9008a58e78bb773aa0c93
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-01-25 11:23:01 +0100

    Update User struct

--- pkg/ftapi/entities.go
+++ pkg/ftapi/entities.go
@@ -6,12 +6,18 @@ import "time"
 type User struct {
 	ID int `json:"id,omitempty"`
 	Login string `json:"login,omitempty"`
-	Email string `json:"email"`
-	FirstName string `json:"first_name"`
-	LastName string `json:"last_name"`
-	Kind string `json:"kind"`
-	CampusID int `json:"campus_id"`
+	Email string `json:"email,omitempty"`
+	FirstName string `json:"first_name,omitempty"`
+	LastName string `json:"last_name,omitempty"`
+	UsualFirstName string `json:"usual_first_name,omitempty"`
+	Phone string `json:"phone,omitempty"`
+	ImageURL string `json:"image_url,omitempty"`
+	IsStaff bool `json:"staff?,omitempty"`
+	Kind string `json:"kind,omitempty"`
+	CampusID int `json:"campus_id,omitempty"`
 	URL string `json:"url,omitempty"`
+	PoolMonth string `json:"pool_month,omitempty"`
+	PoolYear string `json:"pool_year,omitempty"`
 }
 
 type communityService struct {
