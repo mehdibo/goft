b1a3383bf925710e672be707a29270775deba1a8
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-01-29 16:30:34 +0100

    Create users_get.go

---
+++ cmd/users_get.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"goft/pkg/ftapi"
+
+	"github.com/spf13/cobra"
+)
+
+// NewGetUserCmd create the get user cmd
+func NewGetUserCmd(api *ftapi.APIInterface) *cobra.Command {
+	return &cobra.Command{
+		Use:   "get login",
+		Short: "Get details about a user",
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error{
+			user, err := (*api).GetUserByLogin(args[0])
+			if err != nil {
+				return err
+			}
+			if user == nil {
+				return errors.New("failed getting user")
+			}
+			primaryCampus := user.GetPrimaryCampus()
+			_, _ = fmt.Fprintf(cmd.OutOrStdout(), `Id: %d
+Login: %s
+Email: %s
+First name: %s
+Last name: %s
+Phone: %s
+Image: %s
+Is staff: %t
+Pool Month/Year: %s/%s
+`,
+				user.ID,
+				user.Login,
+				user.Email,
+				user.FirstName,
+				user.LastName,
+				user.Phone,
+				user.ImageURL,
+				user.IsStaff,
+				user.PoolMonth,
+				user.PoolYear,
+			)
+			if primaryCampus != nil {
+				_, _ = fmt.Fprintf(cmd.OutOrStdout(), "Primary campus: %s\n", primaryCampus.Name)
+			}
+			return nil
+		},
+	}
+}
+var getCmd = NewGetUserCmd(&API)
+
+func init() {
+	usersCmd.AddCommand(getCmd)
+}
