30eb981b7962b73a1d278c1c8e0ca1a1298acdcd
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-08-02 12:46:51 +0100

    Create requests_delete.go

---
+++ cmd/requests_delete.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/spf13/cobra"
+	"goft/pkg/ftapi"
+	"io/ioutil"
+)
+
+// NewRequestsDeleteCmd Create the post request cmd
+func NewRequestsDeleteCmd(api *ftapi.APIInterface) *cobra.Command {
+	return &cobra.Command{
+		Use:   "delete path",
+		Short: "Send a DELETE request",
+		Long: `Send a DELETE request to path, the path must be the part after /v2/`,
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Print("Enter payload: (press CTRL+D when you finish)\n")
+			var payload string
+			scanner := bufio.NewScanner(cmd.InOrStdin())
+			for scanner.Scan() {
+				line := scanner.Text()
+				payload += line+"\n"
+			}
+			fmt.Println(payload)
+			resp, err := (*api).Delete(args[0], "application/json", bytes.NewReader([]byte(payload)))
+			if err != nil {
+				return err
+			}
+			bodyBytes, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return err
+			}
+			var prettyJSON bytes.Buffer
+			_ = json.Indent(&prettyJSON, bodyBytes, "", "\t")
+			_, _ = fmt.Fprintf(cmd.OutOrStdout(), "%s\n", resp.Status)
+			_, _ = fmt.Fprintf(cmd.OutOrStdout(), "%s\n", prettyJSON.String())
+			return nil
+		},
+	}
+}
+var requestsDeleteCmd = NewRequestsDeleteCmd(&API)
+
+func init() {
+	requestsCmd.AddCommand(requestsDeleteCmd)
+}
