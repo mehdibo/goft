a41a52f38d92a2abba409cbde69e02430e054226
Mehdi Bounya <mehdi.bounya@gmail.com>
2021-10-13 15:00:59 +0100

    Create agu_list

---
+++ cmd/agu_list.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"goft/pkg/ftapi"
+)
+
+// NewAguListCmd Create the agu list cmd
+func NewAguListCmd(api *ftapi.APIInterface) *cobra.Command {
+	return &cobra.Command{
+		Use:   "list login",
+		Short: "List a user's AGUs",
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			agus, err := (*api).GetUserAgus(args[0])
+			if err != nil {
+				return err
+			}
+			for _, agu := range agus {
+				_, _ = fmt.Fprint(cmd.OutOrStdout(), "\n")
+				_, _ = fmt.Fprintf(cmd.OutOrStdout(), "ID: %d\n", agu.ID)
+				_, _ = fmt.Fprintf(cmd.OutOrStdout(), "Reason: %s\n", agu.Reason)
+				_, _ = fmt.Fprintf(cmd.OutOrStdout(), "Is free: %t\n", agu.IsFree)
+				_, _ = fmt.Fprintf(cmd.OutOrStdout(), "Begins at: %s\n", agu.BeginDate)
+				_, _ = fmt.Fprintf(cmd.OutOrStdout(), "Expected end: %s\n", agu.ExpectedEndDate)
+				_, _ = fmt.Fprintf(cmd.OutOrStdout(), "Ends at: %s\n", agu.EndDate)
+				_, _ = fmt.Fprintf(cmd.OutOrStdout(), "Created at: %s\n", agu.CreatedAt)
+			}
+			//bodyBytes, err := ioutil.ReadAll(resp.Body)
+			//if err != nil {
+			//	return err
+			//}
+			//var prettyJSON bytes.Buffer
+			//_ = json.Indent(&prettyJSON, bodyBytes, "", "\t")
+			//_, _ = fmt.Fprintf(cmd.OutOrStdout(), "%s\n", resp.Status)
+			//_, _ = fmt.Fprintf(cmd.OutOrStdout(), "%s\n", prettyJSON.String())
+			return nil
+		},
+	}
+}
+var aguListCmd = NewAguListCmd(&API)
+
+func init() {
+	aguCmd.AddCommand(aguListCmd)
+}
