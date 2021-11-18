1a9722333e89eef6856b3748ebbbaee6d5903344
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-11-18 18:14:13 +0900

    Send/Recieve token through channel

--- cmd/token.go
+++ cmd/token.go
@@ -2,36 +2,36 @@ package cmd
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync"
 
 	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
 )
 
-func genToken(config *oauth2.Config) *oauth2.Token {
+func genToken(config *oauth2.Config) (*oauth2.Token, error) {
+	var token *oauth2.Token
+
 	if viper.GetString("access_token") == "" {
+		tokenChan = make(chan *oauth2.Token, 1)
+
 		httpServerExitDone := &sync.WaitGroup{}
 		httpServerExitDone.Add(1)
 		srv := startHttpServer(httpServerExitDone)
 
 		url := config.AuthCodeURL("")
-
 		fmt.Println("Open this URL")
 		fmt.Println(url)
 
-		for {
-			// TODO: handling with channel
-			if token != nil {
-				if err := srv.Shutdown(context.Background()); err != nil {
-					panic(err)
-				}
-				httpServerExitDone.Wait()
-				break
-			}
+		token = <-tokenChan
+		if err := srv.Shutdown(context.Background()); err != nil {
+			panic(err)
 		}
+		httpServerExitDone.Wait()
 	} else {
 		token = &oauth2.Token{
 			AccessToken:  viper.GetString("access_token"),
@@ -40,7 +40,11 @@ func genToken(config *oauth2.Config) *oauth2.Token {
 			Expiry:       viper.GetTime("expiry_time"),
 		}
 	}
-	return token
+
+	if token == nil {
+		return nil, errors.New("generate token fail")
+	}
+	return token, nil
 }
 
 func saveConfig() {
@@ -57,7 +61,9 @@ func saveConfig() {
 }
 
 func startHttpServer(wg *sync.WaitGroup) *http.Server {
-	srv := &http.Server{Addr: ":4200"}
+	uri := viper.GetString("redirect_uri")
+	uri = strings.TrimPrefix(uri, "http://")
+	srv := &http.Server{Addr: uri}
 	http.HandleFunc("/", LoginRedirectHandler)
 
 	go func() {
@@ -91,8 +97,6 @@ func LoginRedirectHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Fprintf(w, "OAuth Error:%v", err)
 	}
+	tokenChan <- tok
 	fmt.Fprintln(w, "Successfully logged in. Please close the tab.")
-
-	// TODO: handling with channel
-	token = tok
 }
