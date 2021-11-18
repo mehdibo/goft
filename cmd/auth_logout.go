17180571ba8d857aa0428465f081c961028559cc
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-11-18 21:57:10 +0900

    Add auth logout and delete auth refresh, status

--- cmd/auth_logout.go
+++ cmd/auth_logout.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
 	"goft/pkg/ftapi"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // NewLogoutCmd create the get user cmd
@@ -13,7 +17,20 @@ func NewLogoutCmd(api *ftapi.APIInterface) *cobra.Command {
 		Short: "Log out of a 42intra host",
 		Args:  cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			cmd.Print("[Executed] goft auth logout")
+			configMap := viper.AllSettings()
+			delete(configMap, "access_token")
+			delete(configMap, "refresh_token")
+			delete(configMap, "expiry_time")
+			delete(configMap, "token_type")
+			encodedConfig, _ := json.MarshalIndent(configMap, "", " ")
+			err := viper.ReadConfig(bytes.NewReader(encodedConfig))
+			if err != nil {
+				return err
+			}
+			viper.WriteConfig()
+			color.Set(color.FgGreen)
+			cmd.Println("Logged out")
+			color.Set(color.Reset)
 			return nil
 		},
 	}
