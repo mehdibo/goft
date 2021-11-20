24d63d8163019811c4c9be972db4cc59275397e8
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-11-20 21:08:12 +0900

    Add repo list limit

--- cmd/repo_list.go
+++ cmd/repo_list.go
@@ -14,16 +14,18 @@ func NewGetProjectListCmd(api *ftapi.APIInterface) *cobra.Command {
 		Args:  cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			defer saveConfig()
+			var limit int
+			var err error
+			if limit, err = cmd.PersistentFlags().GetInt("limit"); err != nil {
+				return err
+			}
 			me, err := (*api).GetMe()
 			if err != nil {
 				return err
 			}
 			id := me.ID
-			/*
-				filter := map[string]string{
-					"marked": "false",
-				}
-			*/
+			count := 0
+		loop:
 			for i := 1; ; i++ {
 				projects, err := (*api).GetUserProjects(id, nil, nil, i)
 				if err != nil {
@@ -38,6 +40,10 @@ func NewGetProjectListCmd(api *ftapi.APIInterface) *cobra.Command {
 				for _, project := range projects {
 					if len(project.Teams) != 0 {
 						cmd.Println(project.Project.Slug)
+						count++
+					}
+					if count >= limit {
+						break loop
 					}
 				}
 			}
@@ -49,5 +55,6 @@ func NewGetProjectListCmd(api *ftapi.APIInterface) *cobra.Command {
 var getProjectListCmd = NewGetProjectListCmd(&API)
 
 func init() {
+	getProjectListCmd.PersistentFlags().IntP("limit", "L", 5, "Maximum number of repositories to list")
 	projectsCmd.AddCommand(getProjectListCmd)
 }
