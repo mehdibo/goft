49d8b29cab8d6f0bbacc68c73394dd484b0050a3
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-11-22 12:22:10 +0900

    Print error if repogitory not found

--- cmd/browse.go
+++ cmd/browse.go
@@ -13,7 +13,7 @@ import (
 // browseCmd represents the browse command
 func NewBrowseCmd(api *ftapi.APIInterface) *cobra.Command {
 	return &cobra.Command{
-		Use:   "browse <command>",
+		Use:   "browse <project>",
 		Short: "Open the project page in the web browser",
 		Long:  `Open the project page in the web browser`,
 		Args:  cobra.ExactArgs(1),
