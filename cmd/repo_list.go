d94787c5817ef4ad1a4b9ed0240724f984c65bdb
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-11-23 17:48:26 +0900

    Add quiet option to repo list

--- cmd/repo_list.go
+++ cmd/repo_list.go
@@ -14,6 +14,7 @@ func NewGetProjectListCmd(api *ftapi.APIInterface) *cobra.Command {
 		Short: "Show team locked project list",
 		Args:  cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
+
 			var limit int
 			var err error
 			if limit, err = cmd.PersistentFlags().GetInt("limit"); err != nil {
@@ -23,6 +24,15 @@ func NewGetProjectListCmd(api *ftapi.APIInterface) *cobra.Command {
 			if user, err = cmd.PersistentFlags().GetString("user"); err != nil {
 				return err
 			}
+			var isQuiet bool
+			if isQuiet, err = cmd.PersistentFlags().GetBool("quiet"); err != nil {
+				return err
+			}
+
+			if !isQuiet {
+				fmt.Printf("\nShowing %d projects in @%s\n\n", limit, user)
+			}
+
 			count := 0
 		loop:
 			for i := 1; ; i++ {
@@ -33,11 +43,31 @@ func NewGetProjectListCmd(api *ftapi.APIInterface) *cobra.Command {
 				if len(projects) == 0 {
 					break
 				}
+
 				for _, project := range projects {
-					if len(project.Teams) != 0 {
-						fmt.Println(project.Project.Slug)
-						count++
+					if len(project.Teams) == 0 {
+						continue
 					}
+					count++
+
+					team, err := currentTeam(project)
+					if err != nil {
+						return err
+					}
+
+					var repo string
+					if team.RepoURL == "" {
+						repo = "repository not found"
+					} else {
+						repo = team.RepoURL
+					}
+
+					if isQuiet {
+						fmt.Println(project.Project.Slug)
+					} else {
+						fmt.Printf("%-25s %s\n", project.Project.Slug, repo)
+					}
+
 					if count >= limit {
 						break loop
 					}
@@ -53,5 +83,6 @@ var getProjectListCmd = NewGetProjectListCmd(&API)
 func init() {
 	getProjectListCmd.PersistentFlags().IntP("limit", "L", 5, "Maximum number of repositories to list")
 	getProjectListCmd.PersistentFlags().StringP("user", "u", os.Getenv("USER"), "Set specific user")
+	getProjectListCmd.PersistentFlags().BoolP("quiet", "q", false, "Only display project slug")
 	projectsCmd.AddCommand(getProjectListCmd)
 }
